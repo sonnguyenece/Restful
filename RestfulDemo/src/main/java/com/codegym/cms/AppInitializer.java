b1c174a7bcd956207e472291000ca6bd0a73018d
sonnguyenece@gmail.com <sonnguyenece@gmail.com>
2020-06-12 11:52:08 +0700

    update restful

---
+++ RestfulDemo/src/main/java/com/codegym/cms/AppInitializer.java
@@ -0,0 +1,32 @@
+package com.codegym.cms;
+
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import javax.servlet.Filter;
+
+public class AppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{com.codegym.cms.ApplicationConfig.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+
+    @Override
+    protected Filter[] getServletFilters() {
+        CharacterEncodingFilter filter = new CharacterEncodingFilter();
+        filter.setEncoding("UTF-8");
+        filter.setForceEncoding(true);
+        return new Filter[]{filter};
+    }
+
+}
